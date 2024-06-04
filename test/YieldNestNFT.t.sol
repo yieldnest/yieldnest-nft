b1b5a1d49b284d977253df2ba9375103e3a8e011
Tim Denisenko <timothy243@gmail.com>
2024-06-04 18:00:27 +0300

    chore: separate tests

--- test/YieldNestNFT.t.sol
+++ test/YieldNestNFT.t.sol
@@ -11,9 +11,18 @@ import {IERC721Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.s
 import {SigUtils} from "./utils/SigUtils.sol";
 
 contract YieldNestNFTTest is Test {
+    
+    //--------------------------------------------------------------------------------------
+    //-----------------------------------  VARIABLES  --------------------------------------
+    //--------------------------------------------------------------------------------------
+
     YieldNestNFT internal nft;
     YieldNestNFT internal implementation;
     TransparentUpgradeableProxy internal proxy;
+    SigUtils internal sigUtils;
+
+    IYieldNestNFT.MintVoucher internal mintVoucher;
+    IYieldNestNFT.UpgradeVoucher internal upgradeVoucher;
 
     uint256 internal adminPrivateKey;
     uint256 internal minterPrivateKey;
@@ -25,7 +34,9 @@ contract YieldNestNFTTest is Test {
 
     bytes32 internal DOMAIN_SEPARATOR;
 
-    SigUtils internal sigUtils;
+    //--------------------------------------------------------------------------------------
+    //-------------------------------------  SETUP  ----------------------------------------
+    //--------------------------------------------------------------------------------------
 
     function setUp() public {
         // Make private keys
@@ -42,7 +53,7 @@ contract YieldNestNFTTest is Test {
         implementation = new YieldNestNFT();
         proxy = new TransparentUpgradeableProxy(address(implementation), address(this), new bytes(0));
         nft = YieldNestNFT(address(proxy));
-        nft.initialize(admin, minter, "YieldNestNFT", "YieldNestNFT", "https://nft.yieldnest.finance/");
+        nft.initialize(admin, minter, "YieldNestNFT", "ynNFT", "ipfs://nft.yieldnest.finance/");
 
         // Setup SigUtils
         (, string memory name, string memory version, uint256 chainId, address verifyingContract,,) = nft.eip712Domain();
@@ -56,10 +67,21 @@ contract YieldNestNFTTest is Test {
             )
         );
         sigUtils = new SigUtils(DOMAIN_SEPARATOR);
+
+        // Make vouchers
+        mintVoucher = IYieldNestNFT.MintVoucher({recipient: bob, expiresAt: block.timestamp + 15 minutes});
+        upgradeVoucher = IYieldNestNFT.UpgradeVoucher({tokenId: 1, stage: 1, expiresAt: block.timestamp + 15 minutes});
+
+        // Mint a token for upgrade testing
+        vm.prank(minter);
+        nft.safeMint(admin);
     }
 
-    function test_MintWithMinterRole() public {
-        // Not minter
+    //--------------------------------------------------------------------------------------
+    //-------------------------------------  TESTS  ----------------------------------------
+    //--------------------------------------------------------------------------------------
+
+    function test_MintWithMinterRole_NotMinter() public {
         vm.expectRevert(
             abi.encodeWithSelector(
                 IAccessControl.AccessControlUnauthorizedAccount.selector, address(this), nft.MINTER_ROLE()
@@ -67,104 +89,117 @@ contract YieldNestNFTTest is Test {
         );
         nft.safeMint(bob);
         assertEq(nft.balanceOf(bob), 0);
+    }
 
-        // Minter
+    function test_MintWithMinterRole_Minter() public {
         vm.prank(minter);
         nft.safeMint(bob);
         assertEq(nft.balanceOf(bob), 1);
     }
 
-    function test_MintWithVoucher() public {
-        IYieldNestNFT.MintVoucher memory voucher =
-            IYieldNestNFT.MintVoucher({recipient: bob, expiresAt: block.timestamp + 15 minutes});
-
-        // No signature
+    function test_MintWithVoucher_NoSignature() public {
         vm.expectRevert(abi.encodeWithSelector(ECDSA.ECDSAInvalidSignatureLength.selector, 0));
-        nft.safeMint(voucher, new bytes(0));
+        nft.safeMint(mintVoucher, new bytes(0));
         assertEq(nft.balanceOf(bob), 0);
+    }
 
-        // Wrong signature
-        bytes32 digest = sigUtils.getTypedDataHash(voucher);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(bobPrivateKey, digest);
-        vm.expectRevert("Wrong signature");
-        nft.safeMint(voucher, abi.encodePacked(r, s, v));
+    function test_MintWithVoucher_InvalidSignature() public {
+        bytes32 digest = sigUtils.getTypedDataHash(mintVoucher);
+        vm.expectRevert(IYieldNestNFT.InvalidSignature.selector);
+        mintNft(mintVoucher, digest, bobPrivateKey);
         assertEq(nft.balanceOf(bob), 0);
+    }
 
-        // Correct but expired signature
-        voucher.expiresAt = block.timestamp - 1 seconds;
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        vm.expectRevert("Signature has expired");
-        nft.safeMint(voucher, abi.encodePacked(r, s, v));
+    function test_MintWithVoucher_ExpiredVoucher() public {
+        mintVoucher.expiresAt = block.timestamp - 1 seconds;
+        bytes32 digest = sigUtils.getTypedDataHash(mintVoucher);
+        vm.expectRevert(IYieldNestNFT.ExpiredVoucher.selector);
+        mintNft(mintVoucher, digest, minterPrivateKey);
         assertEq(nft.balanceOf(bob), 0);
+    }
 
-        // Correct signature
-        voucher.expiresAt = block.timestamp + 15 minutes;
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        nft.safeMint(voucher, abi.encodePacked(r, s, v));
+    function test_MintWithVoucher_CorrectSignature() public {
+        bytes32 digest = sigUtils.getTypedDataHash(mintVoucher);
+        mintNft(mintVoucher, digest, minterPrivateKey);
         assertEq(nft.balanceOf(bob), 1);
     }
 
-    function test_upgradeWithVoucher() public {
-        IYieldNestNFT.UpgradeVoucher memory voucher =
-            IYieldNestNFT.UpgradeVoucher({tokenId: 1, stage: 1, expiresAt: block.timestamp + 15 minutes});
+    function test_upgradeWithVoucher_NonexistentToken() public {
+        upgradeVoucher.tokenId = 2;
+        bytes32 digest = sigUtils.getTypedDataHash(mintVoucher);
+        vm.expectRevert(abi.encodeWithSelector(IERC721Errors.ERC721NonexistentToken.selector, 2));
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
+        assertEq(nft.stages(2), 0);
+    }
 
-        // Non-existent token
-        vm.expectRevert(abi.encodeWithSelector(IERC721Errors.ERC721NonexistentToken.selector, 1));
-        nft.safeUpgrade(voucher, new bytes(0));
-
-        // Mint a token
-        vm.prank(minter);
-        nft.safeMint(bob);
-
-        // No signature
+    function test_upgradeWithVoucher_NoSignature() public {
         vm.expectRevert(abi.encodeWithSelector(ECDSA.ECDSAInvalidSignatureLength.selector, 0));
-        nft.safeUpgrade(voucher, new bytes(0));
+        nft.safeUpgrade(upgradeVoucher, new bytes(0));
         assertEq(nft.stages(1), 0);
+    }
 
-        // Wrong signature
-        bytes32 digest = sigUtils.getTypedDataHash(voucher);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(bobPrivateKey, digest);
-        vm.expectRevert("Wrong signature");
-        nft.safeUpgrade(voucher, abi.encodePacked(r, s, v));
+    function test_upgradeWithVoucher_InvalidSignature() public {
+        bytes32 digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        vm.expectRevert(IYieldNestNFT.InvalidSignature.selector);
+        upgradeNft(upgradeVoucher, digest, bobPrivateKey);
         assertEq(nft.stages(1), 0);
+    }
 
-        // Expired signature
-        voucher.expiresAt = block.timestamp - 1 seconds;
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        vm.expectRevert("Signature has expired");
-        nft.safeUpgrade(voucher, abi.encodePacked(r, s, v));
+    function test_upgradeWithVoucher_ExpiredVoucher() public {
+        upgradeVoucher.expiresAt = block.timestamp - 1 seconds;
+        bytes32 digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        vm.expectRevert(IYieldNestNFT.ExpiredVoucher.selector);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
         assertEq(nft.stages(1), 0);
+    }
 
-        // Correct signature
-        voucher.expiresAt = block.timestamp + 15 minutes;
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        nft.safeUpgrade(voucher, abi.encodePacked(r, s, v));
+    function test_upgradeWithVoucher_CorrectSignature() public {
+        bytes32 digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
         assertEq(nft.stages(1), 1);
+    }
 
-        // Invalid stage (same stage)
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        vm.expectRevert("Invalid stage");
-        nft.safeUpgrade(voucher, abi.encodePacked(r, s, v));
+    function test_upgradeWithVoucher_SameStage() public {
+        bytes32 digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
         assertEq(nft.stages(1), 1);
+        vm.expectRevert(IYieldNestNFT.InvalidStage.selector);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
+        assertEq(nft.stages(1), 1);
+    }
 
-        // Increase stage
-        voucher.stage = 2;
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        nft.safeUpgrade(voucher, abi.encodePacked(r, s, v));
+    function test_upgradeWithVoucher_IncreaseStage() public {
+        bytes32 digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
+        assertEq(nft.stages(1), 1);
+        upgradeVoucher.stage = 2;
+        digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
         assertEq(nft.stages(1), 2);
+    }
 
-        // Decrease stage
-        voucher.stage = 1;
-        digest = sigUtils.getTypedDataHash(voucher);
-        (v, r, s) = vm.sign(minterPrivateKey, digest);
-        vm.expectRevert("Invalid stage");
+    function test_upgradeWithVoucher_DecreaseStage() public {
+        bytes32 digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
+        assertEq(nft.stages(1), 1);
+        upgradeVoucher.stage = 0;
+        digest = sigUtils.getTypedDataHash(upgradeVoucher);
+        vm.expectRevert(IYieldNestNFT.InvalidStage.selector);
+        upgradeNft(upgradeVoucher, digest, minterPrivateKey);
+        assertEq(nft.stages(1), 1);
+    }
+
+    //--------------------------------------------------------------------------------------
+    //-----------------------------------  INTERNAL  ---------------------------------------
+    //--------------------------------------------------------------------------------------
+
+    function mintNft(IYieldNestNFT.MintVoucher memory voucher, bytes32 digest, uint256 privateKey) internal {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
+        nft.safeMint(voucher, abi.encodePacked(r, s, v));
+    }
+
+    function upgradeNft(IYieldNestNFT.UpgradeVoucher memory voucher, bytes32 digest, uint256 privateKey) internal {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
         nft.safeUpgrade(voucher, abi.encodePacked(r, s, v));
-        assertEq(nft.stages(1), 2);
     }
 }
